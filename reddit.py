f7116ac0b9f94c3bc6ec5ff437b3ffad968413ec
Ian Forbes <ian.forbes@mail.mcgill.ca>
2014-09-18 22:21:25 -0400

    Added the ability to check if we have alread downloaded a thread.

--- reddit.py
+++ reddit.py
@@ -1,4 +1,4 @@
-import praw, json, sys
+import praw, json, sys, os
 
 # Tells the json encoder how to serialize comments and submission.
 def serial(obj):
@@ -12,20 +12,25 @@ def serial(obj):
 r = praw.Reddit('COMP-598 ML BOT'
                 'Url: www.cs.mcgill.ca')
 
-
 r.login('en4bz')
 # Get all top post from this week. Currently the first page (25 posts)
 # Need to set 'limit=x' to get x posts.
 posts = r.get_subreddit(sys.argv[1]).get_top_from_year(limit=100)
 for post in posts:
     print 'pulling comments from', post
-    # Expand all 'more comments'. This is the same as going through all the 
-    # links on the webpage and expanding them. It will only expand links with
-    # with more than 'threshold=2' children.
-    post.replace_more_comments(limit=None, threshold=2)
-    print 'complete'    
-    post_info = {'meta': post.json_dict, 'comments': post.comments}
-    # write to file
-    f = open(str(post), 'w')
-    json.dump(post_info, f, default=serial)
-    f.close()
+    filename = post.json_dict['name'] + '.json'
+    if filename in os.listdir('.'):
+        print 'seen'
+    else:      
+        # Expand all 'more comments'. This is the same as going through all the 
+        # links on the webpage and expanding them. It will only expand links with
+        # with more than 'threshold=2' children.
+        post.replace_more_comments(limit=None, threshold=2)
+        print 'complete'    
+        post_info = {'meta': post.json_dict, 'comments': post.comments}
+        # write to file
+        f = open(filename, 'w')
+        json.dump(post_info, f, default=serial)
+        f.close()
+
+    sys.stdout.flus() # flush stdout so logs get written
