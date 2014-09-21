3e34ff3ec97b39b29a6a9fa7ec0ccce4b825709e
Xavier Denis <xldenis@gmail.com>
2014-09-20 23:36:49 -0400

    output count matrix

--- features.rb
+++ features.rb
@@ -4,13 +4,14 @@ require 'csv'
 
 Encoding.default_external = "utf-8"
 def markdown_strip(md)
- strip = md.gsub(/!?\[(\w*)\]\(\w*\)/,'\1') #links
- strip = strip.gsub(/[**?|__?](.+)[**?|__?]/,'\1') #emphasis
+ strip = md.gsub(/!?\[([^\[\]\(\)]*)\]\(.*\)/,'\1') #links
+ strip = strip.gsub(/\*\*?([^\*]+)\*\*?/,'\1') #emphasis
  strip = strip.gsub(/\#{1,6}\s*(\w+)\n/,'\1')
 end
 
 def tokenize(string)
-  string.split.map {|w| Stemmer::stem_word(w.gsub('.','')).force_encoding('utf-8')}
+  string.downcase.split.map {|w| w.gsub('^',' ').split }.flatten
+  .map {|w| Stemmer::stem_word(w.gsub(/[.|,|!|?|"|:]/,'')).force_encoding('utf-8')}
 end
 
 
@@ -21,27 +22,47 @@ def traverse(node, &trans)
   [trans.call(node)] + (children || []).flatten
 end
 
-comments = JSON.parse(File.read(ARGV[0]).force_encoding('UTF-8'))['comments']
+histogram = Hash.new(0)
+posts = []
+ARGV.each do |file|
+  comments = JSON.parse(File.read(file).force_encoding('UTF-8'))['comments']
 
-filtered = comments.map do |com| 
-  traverse(com) do |comment|
-    body_string = markdown_strip(comment['body'])
-    {
-      author: comment['author'],
-      body: body_string,
-      subreddit: comment['subreddit'],
-      score: comment['score'],
-      tokens: tokenize(body_string),
-    }
+  filtered = comments.map do |com| 
+    traverse(com) do |comment|
+      body_string = markdown_strip(comment['body'])
+      {
+        author: comment['author'],
+        body: body_string,
+        subreddit: comment['subreddit'],
+        score: comment['score'],
+        tokens: tokenize(body_string),
+      }
+    end
+  end.flatten
+
+  filtered.each {|row| row[:tokens].map {|tok| histogram[tok] += 1} }
+  posts << filtered
+end
+
+features = histogram.sort_by {|k,v| v}.last(2500).map {|m| m[0]}
+
+posts.each_with_index do |post,i|
+  uni_mat = post.map do |comment|
+    mini_histo = comment[:tokens].each_with_object(Hash.new(0)) {|w,c| c[w] += 1}
+    features.map {|f| mini_histo[f]}
   end
-end.flatten
-
-puts filtered.to_json
-name = File.basename(ARGV[0],'.json') + "_filtered.csv"
-
-CSV.open(name, 'w') do |csv|
-  filtered.each do |ft|
-    csv <<[ft[:author], ft[:body], ft[:subreddit], ft[:score], ft[:tokens].join(" ")]
+  puts features
+  name = File.basename(ARGV[i],'.json') + "_filtered.csv"
+  CSV.open(name, 'w') do |csv|
+    csv << features
+    uni_mat.map {|u| csv << u}
   end
 end
 # File.open(name,'w:UTF-8') {|f| f.write(filtered.to_json)}
+
+# name = File.basename(file,'.json') + "_filtered.csv"
+#   CSV.open(name, 'w') do |csv|
+#     filtered.each do |ft|
+#       csv <<[ft[:author], ft[:body], ft[:subreddit], ft[:score], ft[:tokens].join(" ")]
+#     end
+#   end
